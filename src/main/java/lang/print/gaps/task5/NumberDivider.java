70675de7c6bc91d61989f3cba5a294d75106d3ef
Hanna_Urban <huan92@yandex.ru>
2024-01-14 20:41:52 +0300

    some corrections are made

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,15 +2,15 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
 
-    static void divide(int toBeDivided)
-   {int stringResult= toBeDivided/5;
-     float floatResult;
-       floatResult = Float.valueOf(stringResult);
-       System.out.println(floatResult);
+    static void divide(float toBeDivided)
+   {float result= toBeDivided/5;
+
+      // floatResult = (float) stringResult;
+       System.out.println(result);
     }
           public static void main (String[]args)
            {
-               int toBeDivided=50;
+           float toBeDivided=50.25f;
           divide(toBeDivided);
        }
 }
